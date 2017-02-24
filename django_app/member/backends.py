a1f841e679af57c46d298599c64e2eabf53c5a42
chumjikim <kcg9077@gmail.com>
2017-02-24 11:32:18 +0900

    Facebook = Facebook Backend 구현

---
+++ django_app/member/backends.py
@@ -0,0 +1,14 @@
+from member.models import MyUser
+
+
+class FacebookBackend():
+    def authenticate(self, facebook_id, **extra_field):
+        user, user_created = MyUser.objects.get_or_create(username=facebook_id)
+        return user
+
+    def get_user(self, user_id):
+        try:
+            return MyUser.objects.get(id=user_id)
+        except MyUser.DoesNotExist:
+            return None
+
