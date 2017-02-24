1294d64a7d1bad66189df89825b4484df6d10ac2
chumjikim <kcg9077@gmail.com>
2017-02-24 09:28:22 +0900

    second commit

--- django_app/ft/tests.py
+++ django_app/ft/tests.py
@@ -2,4 +2,15 @@
 member app을 만들고
 AbstrctaUser를 상속받는 Myuser를 구현
 이후 migration
+
+-로그인 뷰 생성
+view 생성
+def login_fbv(request):
+
+template 연결
+member/login.html
+
+url연결
+urls.py에 member/login/으로 연결되도록
+
 """
